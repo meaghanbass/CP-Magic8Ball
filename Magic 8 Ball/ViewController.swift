9685d797b1b361e65cc59f56638b2a3bd16648be
Meaghan Bass <45584434+meaghanbass@users.noreply.github.com>
2019-05-21 14:57:49 -0700

    inital commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Meaghan Bass on 3/30/19.
+//  Copyright © 2019 Meaghan Bass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
